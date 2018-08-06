789f487ddd763a12f3c1626f133af7a8f646ca75
Nerissa19 <feiyan0319@gmail.com>
2018-08-07 00:41:33 +0800

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -1,19 +1,36 @@
-nei <- readRDS("data/summarySCC_PM25.rds")
-scc <- readRDS("data/Source_Classification_Code.rds")
 
-library('data.table')
-library('ggplot2')
+source("downloadArchive.R")
 
-df <- data.table(nei)
+# Load the NEI & SCC data frames.
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
-# filter dataset to only include Baltimore with type equal to 'ON-ROAD'
-data <- subset(df, fips %in% c('06037', '24510') & type == 'ON-ROAD')
+# Gather the subset of the NEI data which corresponds to vehicles
+vehicles <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
+vehiclesSCC <- SCC[vehicles,]$SCC
+vehiclesNEI <- NEI[NEI$SCC %in% vehiclesSCC,]
 
-by_year <- data[, list(emissions=sum(Emissions)), by=c('year', 'fips')]
-by_year$year = as.numeric(as.character(by_year$year))
-by_year$emissions = as.numeric(as.character(by_year$emissions))
+# Subset the vehicles NEI data by each city's fip and add city name.
+vehiclesBaltimoreNEI <- vehiclesNEI[vehiclesNEI$fips=="24510",]
+vehiclesBaltimoreNEI$city <- "Baltimore City"
 
-ggplot(data=by_year, aes(x=year, y=emissions, col=fips)) + geom_line() + geom_point() + ggtitle("Emissions from Motor Vehicles in Baltimore City compared to Los Angeles")
+vehiclesLANEI <- vehiclesNEI[vehiclesNEI$fips=="06037",]
+vehiclesLANEI$city <- "Los Angeles County"
 
-dev.copy(png, file="plot6.png", width=480, height=480)
+# Combine the two subsets with city name into one data frame
+bothNEI <- rbind(vehiclesBaltimoreNEI,vehiclesLANEI)
+
+png("plot6.png",width=480,height=480,units="px",bg="transparent")
+
+library(ggplot2)
+ 
+ggp <- ggplot(bothNEI, aes(x=factor(year), y=Emissions, fill=city)) +
+ geom_bar(aes(fill=year),stat="identity") +
+ facet_grid(scales="free", space="free", .~city) +
+ guides(fill=FALSE) + theme_bw() +
+ labs(x="year", y=expression("Total PM"[2.5]*" Emission (Kilo-Tons)")) + 
+ labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore & LA, 1999-2008"))
+ 
+print(ggp)
+
 dev.off()
